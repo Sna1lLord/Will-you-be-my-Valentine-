c3cf5a28483db783c3c43d3deeb3bcbf6525122d
Sna1lLord <81821954+Sna1lLord@users.noreply.github.com>
2026-02-11 14:27:36 +0100

    Replace English messages with Swedish translations

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Är du säker?",
+    "Helt säker??",
+    "Hundra?",
+    "Stumpi snälla...",
+    "Tänk över det!",
+    "Emi blir ledsen..",
+    "Väldigt ledsen...",
+    "Väldigt väldigt ledsen...",
+    "Okejdå jag slutar fråga...",
+    "Kommer aldrig sluta fråga! ❤️"
 ];
 
 let messageIndex = 0;
